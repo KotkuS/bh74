a3e2778e7c1fa0813d76b382862107d2dd036e78
KotkuS <kathrin.savko@gmail.com>
2024-02-05 21:32:52 +0300

    (((

--- 4_3.py
+++ 4_3.py
@@ -1,5 +1,2 @@
-indict = {}
-a = indict.setdefault("name", input("Введите имя:"))
-b = indict.setdefault("email", input("Введите почту:"))
-dict_ = {n: indict for n in range(0, 3)}
+dict_ = {i: {"Name": input(f"Name {i}: "), "Email": input(f"Email {i}: ")} for i in range(0, 3)}
 print(dict_)
