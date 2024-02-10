b68a08d2cea5825e7f09ab78665c5376d6dd9dac
KotkuS <kathrin.savko@gmail.com>
2024-02-10 21:21:29 +0300

    (((

--- lesson_6/6_4.py
+++ lesson_6/6_4.py
@@ -1,3 +1,6 @@
 # Дан список содержащий в себе различные типы данных,
 # отфильтровать таким образом, чтобы остались только строки,
 # использование дополнительного списка незаконно
+data_list = ["Hello", 2, "1", (1, 2), {"a": True}, None, "world", 194]
+new_data_list = list(filter(lambda x: isinstance(x, str), data_list))
+print(new_data_list)
