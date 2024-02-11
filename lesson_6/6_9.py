0d671ace2ead5c8eed2c2b4f610c43d89247aa0c
KotkuS <kathrin.savko@gmail.com>
2024-02-11 10:36:22 +0300

    (((

---
+++ lesson_6/6_9.py
@@ -0,0 +1,41 @@
+# Дан словарь словарей, ключ внешнего словаря - id пользователя, значение -
+# словарь с данными о пользователе (имя, фамилия, телефон, почта), вывести
+# имена тех, у кого не указана почта (нет ключа email или значение этого ключа -
+# пустая строка)
+id_dict = {
+    "ID1": [
+        {"Name": "Alex",
+         "SurName": "Smith",
+         "Tel": 6765432,
+         "email": "wertyu@sd.com",
+         }
+    ],
+    "ID2": [
+        {"Name": "Mike",
+         "SurName": "Dilan",
+         "Tel": 654326,
+         "email": "asdgyu@sd.com",
+         }
+    ],
+    "ID3": [
+        {"Name": "Till",
+         "SurName": "Lindemann",
+         "Tel": 124999,
+         "email": "fdaaww@sd.com",
+         }
+    ],
+    "ID4": [
+        {"Name": "Alice",
+         "SurName": "Cooper",
+         "Tel": 7839673,
+         "email": "ddsff@sd.com",
+         }
+    ],
+    "ID5": [
+        {"Name": "Einar",
+         "SurName": "Selvik",
+         "Tel": 1148867,
+         "email": "dsfgyu@sd.com",
+         }
+    ],
+}
