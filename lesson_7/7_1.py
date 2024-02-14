a914a07a684317fe093c61f328417dda392e74fd
KotkuS <kathrin.savko@gmail.com>
2024-02-14 15:45:22 +0300

    (((

--- lesson_7/7_1.py
+++ lesson_7/7_1.py
@@ -5,10 +5,39 @@ class Car:
         self.is_baby_seat = is_baby_seat
         self.is_busy = False
 
+    def __bool__(self):
+        return not self.is_busy
+
     def __str__(self):
         return (f"Color = {self.color}, passenger seats = {self.seats}. "
                 f"Baby seat availability: {self.is_baby_seat}")
 
 
 car_1 = Car("black", 3, False)
-print(str(car_1))
+car_2 = Car("blue", 4, True)
+car_3 = Car("white", 5, True)
+car_4 = Car("green", 3, True)
+car_5 = Car("gray", 4, False)
+
+cars_list = []
+cars_list.append(car_1)
+cars_list.append(car_2)
+cars_list.append(car_3)
+cars_list.append(car_4)
+cars_list.append(car_5)
+
+
+class Taxi:
+    def __init__(self, cars: list[Car]):
+        self.car = cars
+
+    def find_car(self, count_passengers: int, is_baby: bool):
+        if is_baby == 1:
+            count_passengers += 1
+        for i in cars_list:
+            if (i.seats == count_passengers) and i.is_baby_seat is True:
+                return print(i)
+
+
+taxi1 = Taxi(cars_list)
+taxi1.find_car(2, True)
