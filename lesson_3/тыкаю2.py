6f875cd2ccf550d40a0ae1598c0c110b1323fac4
KotkuS <kathrin.savko@gmail.com>
2024-02-16 23:24:25 +0300

    (((

--- lesson_3/тыкаю2.py
+++ lesson_3/тыкаю2.py
@@ -213,7 +213,7 @@
 #     name: str
 #     email: str
 #
-from abc import ABC, abstractmethod
+# from abc import ABC, abstractmethod
 #
 # class AbstractView(ABC):
 #     @abstractmethod
@@ -232,26 +232,80 @@ from abc import ABC, abstractmethod
 #         pass
 
 
-class AbstarctPhone(ABC):
-    @abstractmethod
-    def call(self):
-        pass
+# class AbstarctPhone(ABC):
+#     @abstractmethod
+#     def call(self):
+#         pass
+#
+#
+# class SMSMixin:
+#     def sms(self):
+#         ...
+#
+#
+# class MobilePhone(AbstarctPhone, SMSMixin):
+#     def call(self):
+#         pass
+#
+#     def sms(self):
+#         pass
+#
+#
+# class StaticPhone(AbstarctPhone):
+#     def call(self):
+#         pass
+#
+class Category:
+    categories = []
+
+    @classmethod
+    def add(cls, name: str):
+        if len(cls.categories) == 0:
+            cls.categories.append(name)
+            return print(f"{name}, {cls.categories.index(name)}")
+        try:
+            for _ in cls.categories:
+                if name not in cls.categories:
+                    cls.categories.append(name)
+                    return print(f"{name}, {cls.categories.index(name)}")
+                else:
+                    raise ValueError
+        except ValueError:
+            print(f"Ошибка: данная категория уже есть в списке!")
+
+    @classmethod
+    def get(cls, index: int):
+        try:
+            print(cls.categories[index])
+        except IndexError:
+            print(f"IndexError")
+
+    @classmethod
+    def delete(cls, index):
+        try:
+            cls.categories.pop(index)
+        except IndexError:
+            return None
+
+    @classmethod
+    def update(cls, index: int, new_name: str):
+        try:
+            for _ in cls.categories:
+                if new_name in cls.categories:
+                    raise ValueError
+        except ValueError:
+            print("You are lox")
+        if new_name not in cls.categories and index > len(cls.categories) - 1:
+            cls.categories.append(new_name)
+        if new_name not in cls.categories and index <= len(cls.categories) - 1:
+            cls.categories.insert(index, new_name)
 
 
-class SMSMixin:
-    def sms(self):
-        ...
-
-
-class MobilePhone(AbstarctPhone, SMSMixin):
-    def call(self):
-        pass
-
-    def sms(self):
-        pass
-
-
-class StaticPhone(AbstarctPhone):
-    def call(self):
-        pass
-    
+Category.add("Movie")
+Category.add("Music")
+# Category.add("Movie")
+# Category.get(1)
+# Category.delete(2)
+Category.update(2, "Cartoon")
+Category.update(1, "Car")
+print(Category.categories)
